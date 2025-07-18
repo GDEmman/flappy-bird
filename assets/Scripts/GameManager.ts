d177fa176394254dcc7a2081411e63d1ee9f5e7f
GDEmman <penaflordemmanuel@gmail.com>
2025-07-18 16:17:03 +0800

    Added Pipes.ts and added destroy function

--- assets/Scripts/GameManager.ts
+++ assets/Scripts/GameManager.ts
@@ -1,14 +1,31 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, instantiate, Prefab } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameManager')
 export class GameManager extends Component {
-    start() {
 
-    }
+    @property(Prefab)
+    pipePrefab: Prefab = null!;
+
+    @property(Node)
+    pipeParent: Node = null!;
+
+    private timer = 0;
 
     update(deltaTime: number) {
-        
+        this.timer += deltaTime;
+        if (this.timer > 2.5) {
+            this.spawnTime();
+            this.timer = 0;
+        }
     }
+
+    spawnTime() {
+        const pipe = instantiate(this.pipePrefab);
+        const yOffset = Math.random() * 200 - 100;
+        pipe.setPosition(400, yOffset);
+        this.pipeParent.addChild(pipe);
+    }
+
 }
 
--- assets/Scripts/Pipe.ts
+++ assets/Scripts/Pipe.ts
@@ -1,14 +1,17 @@
-import { _decorator, Component, Node } from 'cc';
+﻿import { _decorator, Component, Node, Vec3, BoxCollider2D, Collider2D, Contact2DType } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Pipe')
 export class Pipe extends Component {
-    start() {
-
-    }
-
     update(deltaTime: number) {
-        
+    const pos = this.node.position;
+    const newX = pos.x - 200 * deltaTime;
+    this.node.setPosition(newX, pos.y);
+
+    if (newX < -1500) {
+        console.log("PIPE DESTROY!");
+        this.node.destroy(); // ← fixed: add parentheses to actually call destroy
     }
+}
 }
 
