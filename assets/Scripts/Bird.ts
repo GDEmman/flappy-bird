c06e361034b5d0652c179d856ddee14c50bfd275
GDEmman <penaflordemmanuel@gmail.com>
2025-07-18 15:02:02 +0800

    Added Bird.ts with rigid body

--- assets/Scripts/Bird.ts
+++ assets/Scripts/Bird.ts
@@ -1,14 +1,19 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, RigidBody2D, Vec2, input, Input, EventTouch } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Bird')
 export class Bird extends Component {
-    start() {
 
+    @property(RigidBody2D)
+    rigidBody: RigidBody2D = null;
+
+    start() {
+        input.on(Input.EventType.TOUCH_START, this.flap, this);
+        //input.on(Input.EventType.MOUSE_DOWN, this.flap, this);
     }
 
-    update(deltaTime: number) {
-        
+    flap() {
+        this.rigidBody.linearVelocity = new Vec2(0, 15);
     }
 }
 
